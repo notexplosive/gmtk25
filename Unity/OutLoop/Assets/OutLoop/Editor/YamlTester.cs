24ab01331b754f6c149f876c7361da6e7c783522
notexplosive <thenotexplosive@gmail.com>
2025-07-31 18:34:42 -0700

    add editor button

---
+++ Unity/OutLoop/Assets/OutLoop/Editor/YamlTester.cs
@@ -0,0 +1,14 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace OutLoop.Editor.Editor
+{
+    public static class YamlTester
+    {
+        [MenuItem("OutLoop/RunYaml")]
+        public static void RunYaml()
+        {
+            Debug.Log("Hello world!");
+        }
+    }
+}
